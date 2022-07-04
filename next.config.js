72e6feaef43ca11ac41ccf54bd2359a3f3211ac4
art-of-michelangelo <omarx816@hotmail.com>
2022-07-04 22:18:01 +0300

    cvbcvb

--- next.config.js
+++ next.config.js
@@ -1,6 +1,22 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: false,
+
+module.exports = (phase, { defaultConfig }) => {
+  return {
+    reactStrictMode: false,
+    ...defaultConfig,
+
+    webpack: (config) => {
+      config.resolve = {
+        ...config.resolve,
+        fallback: {
+          "fs": false,
+          "path": false,
+          "os": false,
+        }
+      }
+      return config
+    },
+  }
 }
 
-module.exports = nextConfig
+
